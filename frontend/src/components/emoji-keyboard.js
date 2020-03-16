543e46bd445ff123617fe4be2cb8003ee3d33026
Liz Davies <sarah.e.e.davies@gmail.com>
2020-03-16 16:59:11 -0400

    slight refact of how presets are stored and intro of break points

--- frontend/src/components/emoji-keyboard.js
+++ frontend/src/components/emoji-keyboard.js
@@ -1,79 +1,76 @@
 import React from 'react';
 import './keyboard.css';
 
+const EN_KEY_MAP ={
+	"0": "🌻",
+	"1": "🌵",
+	"2": "🚗",
+	"3": "🗿",
+	"4": "🥊",
+	"5": "⚽",
+	"6": "🎿",
+	"7": "🐶",
+	"8": "🐱",
+	"9": "🌸",
+	"-": "🎃",
+	"=": "🌲",
+	"q": "🍉",
+	"w": "🌶️",
+	"e": "️🍒",
+	"r": "🎱",
+	"t": "🥇",
+	"y": "🐞",
+	"u": "️🦄",
+	"i": "🐖",
+	"o": "💨",
+	"p": "🌧️",
+	"[": "❄️",
+	"]": "🎀",
+	"a": "🍦",
+	"s": "🍺",
+	"d": "☕",
+	"f": "🎬",
+	"g": "📽️️",
+	"h": "🎵",
+	"j": "👑",
+	"k": "💎",
+	"l": "🏠",
+	";": "💌",
+	"'": "🔑",
+	"\\": "💡",
+	"z": "♣️",
+	"x": "♦️",
+	"c": "♠️",
+	"v": "♥️",
+	"b": "👾",
+	"n": "🤖",
+	"m": "🚀",
+	",": "🌙",
+	".": "☄️",
+	"/": "🧦"
+}
 const ROWS_CONFIG_EN = [
-	[
-		['1','🌵'],
-		['2','🚗'],
-		['3','🗿'],
-		['4','🥊'],
-		['5','⚽'],
-		['6','🎿'],
-		['7','🌲'],
-		['8','🎃'],
-		['9','❄️'],
-		['0','🐶'],
-		['-','🐱'],
-		['=','🐞']
-	],
-	[
-		['q','🍉'],
-		['w','🌶️'],
-		['e','️🍒'],
-		['r','🎱'],
-		['t','🥇'],
-		['y','🌸'],
-		['u','🌻'],
-		['i','🌧️'],
-		['o','💨'],
-		['p','️🦄'],
-		['[','🐖'],
-		[']','🏠']
-	],
-    [
-		['a','🍦'],
-		['s','🍺'],
-		['d','☕'],
-		['f','🎬'],
-		['g','📽️️'],
-		['h','🎵'],
-		['j','💌'],
-		['k','🎀'],
-		['l','👑'],
-		[';','💎'],
-		['\'','🔑'],
-		['\\','💡']
-	],
-    [
-		['z','♣️'],
-		['x','♦️'],
-		['c','♠️'],
-		['v','♥️'],
-		['b','👾'],
-		['n','🤖'],
-		['m','🚀'],
-		[',','🌙'],
-		['.','☄️'],
-		['/','🧦']
-	]
+		['1','2','3','4','5','6','7','8','9','0','-','='],
+		['q','w','e','r','t','y','u','i','o','p','[',']'],
+		['a','s','d','f','g','h','j','k','l',';','\'','\\'],
+		['z','x','c','v','b','n','m',',','.','/']
 ];
 
 
 class EmojiKey extends React.Component{
     render(){
         const {emoji,key_mapping,clickAction} = this.props;
-        return <button className='emoji-key' onClick={(e)=>clickAction(e,key_mapping)}><span className='emoji-char'>{emoji}</span><span className='key-reminder'>{key_mapping}</span></button>
+        return <button className='emoji-key' onClick={(e)=>clickAction(e,key_mapping)}><span className='emoji-char'>{EN_KEY_MAP[key_mapping]}</span><span className='key-reminder'>{key_mapping}</span></button>
     }
 }
 
 class EmojiKeyRow extends React.Component{
     render(){
         let { keys, clickAction } = this.props;
-        let rendered_keys = keys.map((letter_mapping)=>(
-            <EmojiKey key={letter_mapping[0]}
-                key_mapping={letter_mapping[0]}
-                clickAction={clickAction}
-                emoji={letter_mapping[1]}/>
+        let rendered_keys = keys.map((letter)=>(
+            <EmojiKey key={letter}
+                key_mapping={letter}
+                clickAction={clickAction}/>
         ));
         return <div className="emoji-keyboard-row">{rendered_keys}</div>
     }
@@ -82,8 +79,6 @@ class EmojiKeyRow extends React.Component{
 export class EmojiKeyboard extends React.Component{
     constructor(props){
         super()
-        this.key_alias = {}
-        ROWS_CONFIG_EN.forEach((row)=>row.forEach(([key,emoji])=>this.key_alias[key]=emoji));
         this.actionRecord=[]
         this.actionRecord.push({
             time:new Date(),
@@ -92,20 +87,17 @@ export class EmojiKeyboard extends React.Component{
         this.state={
             password_val:""
         }
-        console.log(this.key_alias);
     }
     clickAction(e,key){
-        console.log(key);
         e.preventDefault();
         this.actionRecord.push({
             time:new Date(),
             action:"button_click",
             target:key,
-            alias:this.key_alias[key]
+            alias:EN_KEY_MAP[key]
         });
         let {password_val} = this.state;
         this.setState({password_val:password_val+key})
-        console.log(this.actionRecord[this.actionRecord.length-1]);
     }
     keyPress(e){
         let key = e.target.value.charAt(e.target.value.length-1)
@@ -113,7 +105,7 @@ export class EmojiKeyboard extends React.Component{
             time:new Date(),
             action:"key_press",
             target:key,
-            alias:this.key_alias[key]
+            alias:EN_KEY_MAP[key]
         });
         this.setState({password_val:e.target.value})
         console.log(this.actionRecord[this.actionRecord.length-1]);
@@ -137,8 +129,8 @@ export class EmojiKeyboard extends React.Component{
         return (
             <div>
                 <div className="form-row">
-                    <input type="password" id="emoji-pass-field" onChange={this.keyPress.bind(this)} value={this.state.password_val}/>
-                    <button type="submit">Submit</button>
+                    <input className="text-input" type="password" id="emoji-pass-field" onChange={this.keyPress.bind(this)} value={this.state.password_val}/>
+                    <button className="form-submit" type="submit">Submit</button>
                 </div>
                 <div className="keyboard-en emoji-keyboard">
                     {rendered_rows}
